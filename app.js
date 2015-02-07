1b1667b8c4109ab8f22e1dcdf826ce2ecbf71fd9
Doug Bennett <dben89x@gmail.com>
2015-02-07 15:40:16 -0700

    got music to work, cant play specific songs

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 $(document).ready(function() {
   $.getJSON('data.json', function(data) {
     var $dataTemplate = $('#dataTemplate').html();
@@ -8,12 +7,15 @@ $(document).ready(function() {
     $('.data').html(newHtml);
   });
   $('.container').on('click', '.fa-play', function() {
+    $('audio').each(function(){ this.pause(); });
     $('i').removeClass('fa-stop').addClass('fa-play');
     $(this).removeClass('fa-play').addClass('fa-stop');
-    $('h1').text('Now Playing ' + $(this).attr('value'));
+    var track = $(this).children('#music').get(0).play();
+    $('h1').text('Now Playing: ' + $(this).attr('value'));
   });
   $('.container').on('click', '.fa-stop', function() {
     $(this).removeClass('fa-stop').addClass('fa-play');
     $('h1').text('Select a Song!');
+    $('#music').get(0).pause();
   });
 });
